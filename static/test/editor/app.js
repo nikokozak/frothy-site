25f1a4827a380379ceee3bfa985f7cd601200929
Niko Kozak <nikokozak@gmail.com>
2026-07-10 21:08:53 -0400

    Ship browser editor 0.2.0

--- static/test/editor/app.js
+++ static/test/editor/app.js
@@ -1,4 +1,4 @@
-import { mountEditor } from "./vendor/frothy-editor/0.1.0/index.js";
+import { mountEditor } from "./vendor/frothy-editor/0.2.0/index.js";
 
 const mount = document.getElementById("editor");
 if (!mount) {
